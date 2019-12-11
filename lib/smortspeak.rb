2ab887eabc17747100b07e2590ee0c09366afe64
Student <example@example.com>
2019-12-10 16:35:12 -0800

    fixed it\!

--- lib/smortspeak.rb
+++ lib/smortspeak.rb
@@ -1,21 +1,19 @@
 class String
   def smortspeak
     array = []
-    split_sentence = self.split(' ')
-    split_sentence.each() do |word|
-      unless word.start_with?( "s" || "S" )
-        word.gsub("s" || "S", "z")
-      end
-    end
-    split_sentence.join(" ")
     split_sentence = self.split(//)
-    split_sentence.each() do |letter|
+    split_sentence.each_with_index do |letter, index|
       if (letter === 'e' || letter === "E")
         letter.replace "3"
       elsif (letter === 'o' || letter === "O")
         letter.replace "0"
       elsif (letter === 'I')
         letter.replace "1"
+      elsif (letter === 's' || letter === 'S')
+        if index != 0 && split_sentence[index - 1] != " "
+          puts "#{split_sentence[index - 1]}, #{index}"
+          letter.replace "z"
+        end
       end
     end
     split_sentence.join("")
